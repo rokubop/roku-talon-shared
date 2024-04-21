d636bfdac047426191918cad9960386b6d018aa1
Roku <rokubop@gmail.com>
2024-04-21 11:34:03 -0500

    refactor box model classes

--- ui_builder/ui_builder_test.py
+++ ui_builder/ui_builder_test.py
@@ -4,42 +4,127 @@ from talon.canvas import Canvas
 from talon.skia.canvas import Canvas as SkiaCanvas
 from talon.skia import RoundRect
 from talon.types import Rect, Point2d
-from typing import TypedDict
+from typing import TypedDict, Optional
 from dataclasses import dataclass
 from .ui_builder_helpers import grow_rect
 
+@dataclass
+class BoxModelSpacing:
+    top: int = 0
+    right: int = 0
+    bottom: int = 0
+    left: int = 0
+
+@dataclass
+class BoxModelLayout:
+    margin_spacing: BoxModelSpacing
+    padding_spacing: BoxModelSpacing
+    margin_rect: Rect
+    padding_rect: Rect
+    content_rect: Rect
+
+    def __init__(self, x: int, y: int, margin_spacing: BoxModelSpacing, padding_spacing: BoxModelSpacing):
+        self.margin_spacing = margin_spacing
+        self.padding_spacing = padding_spacing
+        self.margin_rect = Rect(x, y, 0, 0)
+        self.padding_rect = Rect(x + margin_spacing.left, y + margin_spacing.top, 0, 0)
+        self.content_rect = Rect(x + margin_spacing.left + padding_spacing.left, y + margin_spacing.top + padding_spacing.top, 0, 0)
+
+    def grow_content_rect(self, rect: Rect):
+        grow_rect(self.content_rect, rect)
+        self.padding_rect.width = self.content_rect.width + self.padding_spacing.left + self.padding_spacing.right
+        self.padding_rect.height = self.content_rect.height + self.padding_spacing.top + self.padding_spacing.bottom
+        self.margin_rect.width = self.padding_rect.width + self.margin_spacing.left + self.margin_spacing.right
+        self.margin_rect.height = self.padding_rect.height + self.margin_spacing.top + self.margin_spacing.bottom
+
+
+@dataclass
+class Margin(BoxModelSpacing):
+    pass
+
+@dataclass
+class Padding(BoxModelSpacing):
+    pass
+
+def parse_box_model(model_type: BoxModelSpacing, **kwargs) -> BoxModelSpacing:
+    model = model_type()
+    model_name = model_type.__name__.lower()
+    model_name_x = f'{model_name}_x'
+    model_name_y = f'{model_name}_y'
+
+    if model_name in kwargs:
+        all_sides_value = kwargs[model_name]
+        model.top = model.right = model.bottom = model.left = all_sides_value
+
+    if model_name_x in kwargs:
+        model.left = model.right = kwargs[model_name_x]
+    if model_name_y in kwargs:
+        model.top = model.bottom = kwargs[model_name_y]
+
+    for side in ['top', 'right', 'bottom', 'left']:
+        side_key = f'{model_name}_{side}'
+        if side_key in kwargs:
+            setattr(model, side, kwargs[side_key])
+
+    return model
+
 class UIOptionsDict(TypedDict):
-    margin: int
-    padding: int
-    color: str
-    bg_color: str
     align: str
-    width: int
-    height: int
+    background_color: str
+    border_color: str
+    border_radius: int
+    border_width: int
+    bottom: int
+    color: str
     flex_direction: str
+    height: int
     justify: str
+    left: int
+    margin: Margin
+    padding: Padding
+    right: int
+    top: int
+    width: int
 
 class UITextOptionsDict(UIOptionsDict):
     size: int
     bold: bool
 
-@dataclass
 class UIOptions:
-    margin: int = 0
-    padding: int = 0
-    color: str = "white"
-    bg_color: str = "black"
     align: str = "start"
-    justify: str = "start"
-    width: int = 0
-    height: int = 0
+    background_color: str = None
+    border_color: str = "red"
+    border_radius: int = 0
+    border_width: int = 0
+    bottom: Optional[int] = None
+    top: Optional[int] = None
+    left: Optional[int] = None
+    right: Optional[int] = None
+    color: str = "white"
     flex_direction: str = "column"
     gap: int = 16
+    height: int = 0
+    justify: str = "start"
+    margin: Margin = Margin(0, 0, 0, 0)
+    padding: Padding = Padding(0, 0, 0, 0)
+    width: int = 0
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+
+        self.padding = parse_box_model(Padding, **{k: v for k, v in kwargs.items() if 'padding' in k})
+        self.margin = parse_box_model(Margin, **{k: v for k, v in kwargs.items() if 'margin' in k})
 
 @dataclass
 class UITextOptions(UIOptions):
     size: int = 16
     bold: bool = False
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
 
 class Cursor:
     def __init__(self):
@@ -82,34 +167,30 @@ class UIWithChildren:
 class UIContainer(UIWithChildren):
     def __init__(self, options: UIOptions = None):
         super().__init__(options)
-        self.content_rect = None
-        self.padding_rect = None
-        self.margin_rect = None
+        self.box_model: BoxModelLayout = None
 
     def virtual_render(self, c: SkiaCanvas, cursor: Cursor):
-        self.margin_rect = Rect(cursor.virtual_x, cursor.virtual_y, 0, 0)
-        cursor.virtual_move_to(cursor.virtual_x + self.options.margin, cursor.virtual_y + self.options.margin)
-        self.padding_rect = Rect(cursor.virtual_x, cursor.virtual_y, 0, 0)
-        cursor.virtual_move_to(cursor.virtual_x + self.options.padding, cursor.virtual_y + self.options.padding)
-        self.content_rect = Rect(cursor.virtual_x, cursor.virtual_y, 0, 0)
+        self.box_model = BoxModelLayout(cursor.virtual_x, cursor.virtual_y, self.options.margin, self.options.padding)
+        cursor.virtual_move_to(self.box_model.content_rect.x, self.box_model.content_rect.y)
         for child in self.children:
             rect = child.virtual_render(c, cursor)
             if self.options.flex_direction == "column":
                 cursor.virtual_move_to(cursor.virtual_x, cursor.virtual_y + rect.height + self.options.gap)
             elif self.options.flex_direction == "row":
                 cursor.virtual_move_to(cursor.virtual_x + rect.width + self.options.gap, cursor.virtual_y)
-            grow_rect(self.content_rect, rect)
-        self.padding_rect.width = self.content_rect.width + self.options.padding * 2
-        self.padding_rect.height = self.content_rect.height + self.options.padding * 2
-        self.margin_rect.width = self.padding_rect.width + self.options.margin * 2
-        self.margin_rect.height = self.padding_rect.height + self.options.margin * 2
-        return self.margin_rect
+            self.box_model.grow_content_rect(rect)
+
+        return self.box_model.margin_rect
 
     def render(self, c: SkiaCanvas, cursor: Cursor):
-        cursor.move_to(self.padding_rect.x, self.padding_rect.y)
-        c.paint.color = self.options.bg_color
-        c.draw_rrect(RoundRect.from_rect(self.padding_rect))
-        cursor.move_to(self.content_rect.x, self.content_rect.y)
+        cursor.move_to(self.box_model.padding_rect.x, self.box_model.padding_rect.y)
+        if self.options.background_color:
+            c.paint.color = self.options.background_color
+            if self.options.border_radius:
+                c.draw_rrect(RoundRect.from_rect(self.box_model.padding_rect, x=self.options.border_radius, y=self.options.border_radius))
+            else:
+                c.draw_rect(self.box_model.padding_rect)
+        cursor.move_to(self.box_model.content_rect.x, self.box_model.content_rect.y)
 
         for child in self.children:
             rect = child.render(c, cursor)
@@ -117,7 +198,7 @@ class UIContainer(UIWithChildren):
                 cursor.move_to(cursor.x, cursor.y + rect.height + self.options.gap)
             elif self.options.flex_direction == "row":
                 cursor.move_to(cursor.x + rect.width + self.options.gap, cursor.y)
-        return self.margin_rect
+        return self.box_model.margin_rect
 
 class UIText:
     def __init__(self, text: str, options: UITextOptions = None):
@@ -125,18 +206,29 @@ class UIText:
         self.text = text
         self.text_width = None
         self.text_height = None
+        self.box_model = None
 
     def virtual_render(self, c: SkiaCanvas, cursor: Cursor):
+        self.box_model = BoxModelLayout(cursor.virtual_x, cursor.virtual_y, self.options.margin, self.options.padding)
+        cursor.virtual_move_to(self.box_model.content_rect.x, self.box_model.content_rect.y)
         c.paint.textsize = self.options.size
         self.text_width = c.paint.measure_text(self.text)[1].width
         self.text_height = c.paint.measure_text("E")[1].height
-        return Rect(cursor.virtual_x, cursor.virtual_y, self.text_width, self.text_height)
+        self.box_model.grow_content_rect(Rect(cursor.virtual_x, cursor.virtual_y, self.text_width, self.text_height))
+        return self.box_model.margin_rect
 
     def render(self, c: SkiaCanvas, cursor: Cursor):
+        if self.options.background_color:
+            c.paint.color = self.options.background_color
+            if self.options.border_radius:
+                c.draw_rrect(RoundRect.from_rect(self.box_model.padding_rect, x=self.options.border_radius, y=self.options.border_radius))
+            else:
+                c.draw_rect(self.box_model.padding_rect)
+        cursor.move_to(self.box_model.content_rect.x, self.box_model.content_rect.y)
         c.paint.color = self.options.color
         c.paint.textsize = self.options.size
         c.draw_text(self.text, cursor.x, cursor.y + self.text_height)
-        return Rect(cursor.x, cursor.y, self.text_width, self.text_height)
+        return self.box_model.margin_rect
 
 class UIBuilder(UIWithChildren):
     def __init__(self, **kwargs: UIOptionsDict):
@@ -179,25 +271,22 @@ class Actions:
         global builder
         builder = actions.user.ui_builder()
         window = builder.add_container(
-            # width=100,
-            # height=100,
-            # padding=32,
-            bg_color="222222",
-            # align="center",
-            # flex_direction="column",
-            # justify="between"
+            padding=16,
+            background_color="222222",
+            border_radius=4,
         )
         box = window.add_container(
-            bg_color="444444",
-            padding=16,
-            margin=16,
+            background_color="444444",
+            border_radius=4,
+            padding_y=16,
+            padding_x=32,
         )
-        box.add_text("Hello World!")
+        box.add_text("Hello World!", size=32, padding_top=16, color="dd6666")
         box.add_text("Goodbye World!")
+        box.add_text("Click Me!", background_color="666666", padding_y=16, padding_x=32, size=24, color="blue")
         builder.show()
 
     def ui_builder_test_hide():
         """ui_builder_test"""
         global builder
         builder.hide()
-        # window.add_text("Hello World!")
